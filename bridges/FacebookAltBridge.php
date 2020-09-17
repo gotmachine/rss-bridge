efae77bdc74c3e47745a2bfc861c294c1f6646f7
gotmachine <24925209+gotmachine@users.noreply.github.com>
2020-09-17 22:04:10 +0200

    Update FacebookAltBridge.php

--- bridges/FacebookAltBridge.php
+++ bridges/FacebookAltBridge.php
@@ -659,7 +659,7 @@ EOD;
 							$title = substr($title, 0, strpos(wordwrap($title, 64), "\n")) . '...';
 						
 						// get uri from embeded content
-						$uri = $post->find('._52c6')[0]->getAttribute('href');
+						$uri = $post->find('._52c6')[0]->unescape_fb_link($content)->getAttribute('href');
 						
 						if (empty($uri))
 						{
